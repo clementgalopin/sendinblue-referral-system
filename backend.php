5038f3e678f6641eae06cd2e8fd6d2e16c5145c6
Clément Galopin <clementgalopin@users.noreply.github.com>
2018-02-23 15:40:21 +0100

    Update to SiB's V3 API

--- backend.php
+++ backend.php
@@ -1,96 +1,93 @@
-﻿<?php
+<?php
 
-require('Mailin.php');
-
+/* Indiquez le chemin du dossier vers lequel vous avez stocké l'API Sendinblue */	
+require_once('./APIv3-php-library-master/autoload.php');
 /* Remplacez ici l'ID de liste par l'ID de la vôtre */	
-$list_id = 6;
-/* Remplacez ici l'ID du template liste par l'ID du vôtre */	
-$template_id = 2;
+$list_id = "";
 /* Renseignez ici votre CLÉ API */	
-$API_key = "VOTRE CLE";
+$API_key = "";
 
-$mailin = new Mailin("https://api.sendinblue.com/v2.0", $API_key, 5000);
+/*///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+                                                     NE PAS MODIFIER SOUS CETTE LIGNE
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////*/
 
+/* Connexion à Sendinblue */
+SendinBlue\Client\Configuration::getDefaultConfiguration()->setApiKey('api-key', $API_key);
+$api_instance = new SendinBlue\Client\Api\ContactsApi();
+
+/* Fonction pour lire les objets protégés renvoyés par l'API Sendinblue */ 
+function _r($o){
+	$obj  = new ReflectionObject($o);
+	$r_obj = $obj->getProperty('container');
+	$r_obj->setAccessible(TRUE);
+	return $r_obj->getValue($o);
+}
+
+//////////////////////////////////////////////////////* LE SCRIPT EN LUI-MÊME //////////////////////////////////////////////////////*/
+
+/* Si une adresse de parrain a bien été spécifiée... */
 if( isset($_POST['parrain']) ){
+
+
+	/* On récupère les informations des contacts de la liste */
+	$modifiedSince = new \DateTime("2013-10-20T19:20:30+01:00"); // On choisit une date minimale d'enregistrement lointaine
+	$limit = 500; // On définit le nombre maximum d'utilisateurs retournés à 500
+	$offset = 0; // On définit le numéro du premier contact à 0
+	try { $list_contacts = $api_instance->getContactsFromList($list_id, $modifiedSince, $limit, $offset); } catch (Exception $e) { echo "Erreur lors de la récupération des contacts de la liste"; die(); }
 	
+	/* On créé un tableau contenant les emails de cette liste */
+	$list_emails = array();
+	foreach(_r($list_contacts)["contacts"] as $c => $p){ $list_emails[] = $p["email"]; }
+
 	/* On recupère les données du parrain... */
 	$parrain_email = $_POST['parrain'];	
-	/* ...et on vérifie s'il existe dans la liste Sendinblue */
-	$parrain_data = $mailin->get_user( array("email"=>$parrain_email) );
-
-	if( isset($_POST['invite']) ){
-
-		/* On recupère les données de l'invité... */
-		$invite_email = $_POST['invite'];
-		/* ...et on vérifie s'il existe dans la liste Sendinblue */
-		$invite_data = $mailin->get_user( array("email"=>$invite_email) );
 	
-		/* 
-		Si l'adresse email de l'invité existe dans la liste Sendinblue,
-		cela veut dire qu'elle a déjà été invitée, 
-		on stoppe donc le processus. 
-		*/
-		if( $invite_data["code"] == "success" ){ echo "Cette personne a d&eacute;j&agrave; &eacute;t&eacute; invit&eacute;e."; die(); }
+	/* ...et on vérifie s'il existe dans la liste Sendinblue */
+	if( in_array($parrain_email, $list_emails) ){
+		
+		/* Si une adresse à inviter a bien été spécifiée... */
+		if( isset($_POST['invite']) ){
+			/* ...on recupère les données de l'invité... */
+			$invite_email = $_POST['invite'];
+
+			/* 
+			Si l'adresse email de l'invité existe dans la liste Sendinblue,
+			cela veut dire qu'elle a déjà été invitée, 
+			on stoppe donc le processus. 
+			*/
+			if( in_array($invite_email, $list_emails) ){ echo "Cette personne a d&eacute;j&agrave; &eacute;t&eacute; invit&eacute;e"; die(); }
+					
+			/* On récupère les attributs du parrain */		
+			try {
+				$parrain_data = $api_instance->getContactInfo($parrain_email);
+			
+				/* ...On ajoute l'invité à la liste en spécifiant l'email de son parrain */
+				$invite_create = array( 
+					"email" => $invite_email,
+					"attributes" => array("REF"=>$parrain_email), 				
+					"listIds" => array($list_id)  
+				);				
+				try { $api_instance->createContact($invite_create); } catch (Exception $e) { echo "L'enregistrement de l'adresse email &agrave; inviter a &eacute;chou&eacute; : ", $e->getMessage(), PHP_EOL; die(); }
+		
+				/* On récupère le nombre de parrainages que le parrain a déjà effectués... */
+				$parrain_data_PAR = _r($parrain_data)["attributes"]["PAR"];			
+				if( !is_numeric($parrain_data_PAR) ) { $parrain_data_PAR = 0; }
 				
-		/*
-		Si on a bien récupéré les données du parrain dans la liste Sendinblue...
-		*/
-		if( $parrain_data["code"] == "success" ){
-			
-			/* ...On ajoute l'invité à la liste en spécifiant l'email de son parrain */
-			$invite_create = $mailin->create_update_user( 
-				array( 
-					"email" => $invite_email, 
-					"attributes" => array("REF"=>$parrain_email, 				
-						"listid" => array($list_id) 
-					) 
-				)
-			);
-			
-			/* On récupère le nombre de parrainages que le parrain a déjà effectués... */
-			$parrain_data_PAR = $parrain_data["data"]["attributes"]["PAR"];
-			
-			/*... et on l'incrémente de 1 */
-		    $parrain_update = $mailin->create_update_user( 
-				array( 
-					"email" => $parrain_email, 
-					"attributes" => array("PAR"=>$parrain_data_PAR+1) 
-				) 
-			);
-			
-		/*
-		Si n'a pas réussi à récupérer les données du parrain dans la liste Sendinblue...
-		*/		
-		} else {
-			echo "Cette adresse de parrainage n'existe pas."; die();
+				/*... et on l'incrémente de 1 */		
+				$parrain_update = array( "attributes" => array("PAR"=>$parrain_data_PAR+1) );
+				try { $api_instance->updateContact($parrain_email, $parrain_update); } catch (Exception $e) { echo "La mise &agrave; jour du nombre de parrainages a &eacute;chou&eacute; : ", $e->getMessage(), PHP_EOL; die(); }
+							
+			/*
+			Si n'a pas réussi à récupérer les données du parrain dans la liste Sendinblue...
+			*/		
+			} catch (Exception $e) { echo "Erreur lors de la r&eacute;cup&eacute;ration des donn&eacute;es du parrain : ", $e->getMessage(), PHP_EOL; die(); }
 		}
+		else { echo "Aucun email à inviter n'a &eacute;t&eacute; sp&eacute;cifi&eacute;."; die(); }
 	}
-	else { 
-		echo "Aucun email à inviter n'a &eacute;t&eacute; sp&eacute;cifi&eacute;."; die();
-	}
+	else { echo "Cette adresse de parrainage n'existe pas."; die(); }
 }
-else {
-	echo "Aucun email de parrain n'a &eacute;t&eacute; sp&eacute;cifi&eacute;."; die();
-}
+else { echo "Aucun email de parrain n'a &eacute;t&eacute; sp&eacute;cifi&eacute;."; die(); }
 
-/* ...Si l'ajout de l'email de l'invité à la liste a échoué... */	
-if( !$invite_create["code"] == "success" ){ 
-	echo "L'enregistrement de l'adresse email &agrave; inviter a &eacute;chou&eacute;."; die();
-}
+echo $invite_email." a &eacute;t&eacute; invit&eacute; avec succ&egrave;s !" 
 
-/* ...Si la mise à jour des parrainages du parrain a échoué... */	
-if( !$invite_create["code"] == "success" ){ 
-	echo "La mise &agrave; jour du nombre de parrainages a &eacute;chou&eacute;."; die();
-}
-
-/* On prépare l'email qui va être envoyé à l'invité */	
-$data = array( "id" => $template_id,
-  "to" => $invite_email,
-  "headers" => array("Content-Type"=> "text/html;charset=iso-8859-1", "X-param1"=> "value1", "X-param2"=> "value2", "X-Mailin-custom"=>"my custom value","X-Mailin-tag"=>"my tag value")
-);
-
-/* On envoit l'email */
-$mailin->send_transactional_template($data);
-
-echo $invite_email." a &eacute;t&eacute; invit&eacute; avec succ&egrave;s !;" 
 ?>
